20b3e82097f9f82311c4581ee07dc715ae203254
Mauryn24 <maurynnyakio19@gmail.com>
2024-04-20 16:59:04 +0300

    db

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models  # Importing the models module from Django
+from django.utils import timezone  # Importing the timezone module from Django
+from django.contrib.auth.models import User  # Importing the User model from Django's authentication system
 
 # Create your models here.
+class Post(models.Model):
+    # Defining the Post model as a subclass of models.Model
+    title = models.CharField(max_length=100)  # Title field for the post, limited to 100 characters
+    content = models.TextField()  # Content field for the post, allowing for larger text
+    date_posted = models.DateTimeField(default=timezone.now)  # Date and time when the post was created
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # ForeignKey relationship with the User model, each post belongs to a user
+    # on_delete=models.CASCADE ensures that if the associated user is deleted, all related posts are also deleted
+
+    def __str__(self):
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render  # Importing the render function from Django shortcuts
+from django.http import HttpResponse  # Importing the HttpResponse class from Django HTTP
+from .models import Post  # Importing the Post model from the current directory's models module
 
+# Sample data for demonstration (temporary)
 posts = [
     {
         'author': 'MaurynG',
@@ -15,12 +17,17 @@ posts = [
         'date_posted': 'August 29, 2020'
     }
 ]
+
 # Create your views here.
+
 def home(request):
+    # Retrieve all posts from the database using the Post model
     context = {
-        'posts': posts
+        'posts': Post.objects.all()  # Querying all Post objects from the database
     }
+    # Render the home.html template with the retrieved posts
     return render(request, 'blog/home.html', context)
 
 def about(request):
+    # Render the about.html template with the title 'About page'
     return render(request, 'blog/about.html', {'title': 'About page'})
