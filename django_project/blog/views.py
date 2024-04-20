22b17520d2c5a3469b45e6d196fb5cd2e4ecd769
Mauryn24 <maurynnyakio19@gmail.com>
2024-04-20 17:14:40 +0300

    db

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render  # Importing the render function from Django shortcuts
-from django.http import HttpResponse  # Importing the HttpResponse class from Django HTTP
 from .models import Post  # Importing the Post model from the current directory's models module
 
-# Sample data for demonstration (temporary)
-posts = [
-    {
-        'author': 'MaurynG',
-        'title': 'Blog Post 1',
-        'content': 'My First Content',
-        'date_posted': 'August 28, 2020'
-    },
-    {
-        'author': 'MwendeG',
-        'title': 'Blog Post 2',
-        'content': 'My Second Content',
-        'date_posted': 'August 29, 2020'
-    }
-]
 
 # Create your views here.
 
